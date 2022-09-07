13caacfdcb08f029fb28019e496ad0853f372f4a
root <root@Nikki.ngqgiuzvx10ebeglkznj4h3xsd.rx.internal.cloudapp.net>
2022-09-07 13:50:45 +0000

    Add extras for m30sxx-user 11 RP1A.200720.012 M307FNXXS4CVC1 release-keys
    Signed-off-by: root <root@Nikki.ngqgiuzvx10ebeglkznj4h3xsd.rx.internal.cloudapp.net>

---
+++ README.md
@@ -0,0 +1,21 @@
+## m30sxx-user 11 RP1A.200720.012 M307FNXXS4CVC1 release-keys
+- Manufacturer: samsung
+- Platform: universal9611
+- Codename: m30s
+- Brand: samsung
+- Flavor: m30sxx-user
+- Release Version: 11
+- Id: RP1A.200720.012
+- Incremental: M307FNXXS4CVC1
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-GB
+- Screen Density: undefined
+- Fingerprint: samsung/m30sxx/m30s:11/RP1A.200720.012/M307FNXXS4CVC1:user/release-keys
+- OTA version: 
+- Branch: m30sxx-user-11-RP1A.200720.012-M307FNXXS4CVC1-release-keys
+- Repo: samsung_m30s_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cat system/system/app/Maps/Maps.apk.* 2>/dev/null >> system/system/app/Maps/Maps.apk
+rm -f system/system/app/Maps/Maps.apk.* 2>/dev/null
+cat system/system/app/YouTube/YouTube.apk.* 2>/dev/null >> system/system/app/YouTube/YouTube.apk
+rm -f system/system/app/YouTube/YouTube.apk.* 2>/dev/null
+cat system/system/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> system/system/app/WebViewGoogle/WebViewGoogle.apk
+rm -f system/system/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat system/system/priv-app/AREmoji/AREmoji.apk.* 2>/dev/null >> system/system/priv-app/AREmoji/AREmoji.apk
+rm -f system/system/priv-app/AREmoji/AREmoji.apk.* 2>/dev/null
+cat system/system/priv-app/DualOutFocusViewer_R/DualOutFocusViewer_R.apk.* 2>/dev/null >> system/system/priv-app/DualOutFocusViewer_R/DualOutFocusViewer_R.apk
+rm -f system/system/priv-app/DualOutFocusViewer_R/DualOutFocusViewer_R.apk.* 2>/dev/null
+cat system/system/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> system/system/priv-app/Velvet/Velvet.apk
+rm -f system/system/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat system/system/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> system/system/priv-app/GmsCore/GmsCore.apk
+rm -f system/system/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat system/system/priv-app/SamsungCoreServices/SamsungCoreServices.apk.* 2>/dev/null >> system/system/priv-app/SamsungCoreServices/SamsungCoreServices.apk
+rm -f system/system/priv-app/SamsungCoreServices/SamsungCoreServices.apk.* 2>/dev/null
+cat system/system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk.* 2>/dev/null >> system/system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk
+rm -f system/system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk.* 2>/dev/null
+cat system/system/priv-app/AvatarEmojiSticker_Palette/AvatarEmojiSticker_Palette.apk.* 2>/dev/null >> system/system/priv-app/AvatarEmojiSticker_Palette/AvatarEmojiSticker_Palette.apk
+rm -f system/system/priv-app/AvatarEmojiSticker_Palette/AvatarEmojiSticker_Palette.apk.* 2>/dev/null
+cat system/system/priv-app/SecSettings/SecSettings.apk.* 2>/dev/null >> system/system/priv-app/SecSettings/SecSettings.apk
+rm -f system/system/priv-app/SecSettings/SecSettings.apk.* 2>/dev/null
+cat recovery.img.* 2>/dev/null >> recovery.img
+rm -f recovery.img.* 2>/dev/null
+cat product/app/YandexSearchforAndroid11/YandexSearchforAndroid11.apk.* 2>/dev/null >> product/app/YandexSearchforAndroid11/YandexSearchforAndroid11.apk
+rm -f product/app/YandexSearchforAndroid11/YandexSearchforAndroid11.apk.* 2>/dev/null
